72651ba16ab96e74f650fdc63c3e4b9c5cffe150
rubaqeshta04 <qeshtaruba@gmail.com>
2026-01-15 13:33:05 +0200

    Delete Artical.jsx and update Button and Side components

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,8 @@
-function Button({ name, icon, img }) {
+function Button({ name, children }) {
   return (
     <button className="bg-[#770071] text-white font-bold p-2 rounded-xl hover:bg-purple-400 hover:text-black">
-      <h1>{name}</h1>
-      <p>{icon}</p>
-      <img src={img} className="w-[30px]" />
+      {name}
+      {children}
     </button>
   );
 }
--- src/components/Side/Side.jsx
+++ src/components/Side/Side.jsx
@@ -2,14 +2,31 @@ import Button from "../Button/Button";
 
 function Side() {
   return (
-    <div className="border-4 border-teal-800 border-solid text-center p-5 m-6.25px w-70 flex flex-wrap gap-4">
-      <Button name="جديدة" icon="❤️❤️❤️❤️❤️" />
-      <Button name="الاكثر قراءة" img="./../../../public/assets/19.jpeg" />
-      <Button
-        name="مقالات مميزة"
-        icon="💕💕💕"
-        img="./../../../public/assets/19.jpeg"
-      />
+    <div className="border-4 border-teal-800 border-solid text-center p-5 m-[6.25px] inline-flex flex-wrap gap-4">
+      <Button name="جديدة">
+        <div className="flex flex-col items-center">
+          <span>😊😊😊</span>
+          <img src="../../../public/assets/19.jpeg" className="w-30" />
+        </div>
+      </Button>
+      <Button name="جديدة">
+        <div className="flex flex-col items-center">
+          <span>😊😊😊</span>
+          <img src="../../../public/assets/19.jpeg" className="w-30" />
+        </div>
+      </Button>
+      <Button name="جديدة">
+        <div className="flex flex-col items-center">
+          <span>😊😊😊</span>
+          <img src="../../../public/assets/19.jpeg" className="w-30" />
+        </div>
+      </Button>
+      <Button name="جديدة">
+        <div>
+          <span>😊😊😊</span>
+          <img src="../../../public/assets/19.jpeg" className="w-30" />
+        </div>
+      </Button>
     </div>
   );
 }
